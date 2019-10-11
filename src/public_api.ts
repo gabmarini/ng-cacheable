56400c8f5760621c9489f2cba01b256910bf7341
Gabriele <ga.marini@reply.it>
2019-10-11 10:37:10 +0200

    aggiunto VoidLogger alle public-api.ts

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,4 +1,6 @@
 export * from './decorators/cacheable.decorator';
 export * from './interfaces/Icacheable-metadata.interface';
 export * from './configurations/cache-configuration.configuration';
+export * from './constants/defaults/default-logger.logger';
+
 
